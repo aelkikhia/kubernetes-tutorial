e68a1860d09d18c27ad15b4a19525dad7003a837
aelkikhia <aelkikhia@gmail.com>
2021-07-07 19:29:49 -0500

    initit commit configs and cheat sheet mods adding secret support dashboard and ingress completion service account encrypted travis

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+# build docker images
+# docker build --platform linux/amd64,linux/arm64 -t aelkikhia/multi-client:latest -t aelkikhia/multi-client:$SHA -f ./client/Dockerfile ./client
+# docker build --platform linux/amd64,linux/arm64 -t aelkikhia/multi-server:latest -t aelkikhia/multi-server:$SHA -f ./server/Dockerfile ./server
+# docker build --platform linux/amd64,linux/arm64 -t aelkikhia/multi-worker:latest -t aelkikhia/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker build  -t aelkikhia/multi-client:latest -t aelkikhia/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build  -t aelkikhia/multi-server:latest -t aelkikhia/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build  -t aelkikhia/multi-worker:latest -t aelkikhia/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+# publish to dockerhub
+docker push aelkikhia/multi-client:latest
+docker push aelkikhia/multi-client:$SHA
+docker push aelkikhia/multi-server:latest
+docker push aelkikhia/multi-server:$SHA
+docker push aelkikhia/multi-worker:latest
+docker push aelkikhia/multi-worker:$SHA
+
+# apply k8s configs
+kubectl apply -f k8s
+
+# apply updated images
+kubectl set image deployments/client-deployment client=aelkikhia/multi-client:$SHA
+kubectl set image deployments/server-deployment server=aelkikhia/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=aelkikhia/multi-worker:$SHA
