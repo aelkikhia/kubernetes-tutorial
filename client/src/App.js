27f742e4214a40cc008ea22106b02daab1bf6b31
aelkikhia <aelkikhia@gmail.com>
2021-07-13 21:36:41 -0500

    specify shell type and quotes around variables remove resource limits multi builder I hope

--- client/src/App.js
+++ client/src/App.js
@@ -11,16 +11,11 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React 3
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
+          Fibonacci Calculator on Kubernetes powered by react 
           </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/">Home </Link>
+          <Link to="/otherpage"> Other Page</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
